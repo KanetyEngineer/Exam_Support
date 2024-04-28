53489649350e497d4dc6967f9f409eddb5a48f3c
KanetyEngineer <110831939+KanetyEngineer@users.noreply.github.com>
2024-04-28 21:00:39 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+{
+    const update = () => {
+      // 日本時間の timezone 付きで指定
+      const targetTime = new Date('2025-01-18T10:00+09:30').getTime();
+      const diff = targetTime - Date.now();
+      const numbers = [
+        diff / 1000 / 60 / 60 / 24,
+        (diff / 1000 / 60 / 60) % 24,
+        (diff / 1000 / 60) % 60,
+        (diff / 1000) % 60,
+      ];
+      Array.from(document.querySelectorAll('#container > .num'), (elm, i) => {
+        elm.textContent = `${Math.floor(numbers[i])}`.padStart(2, 0);
+      });
+    };
+    update();
+    setInterval(update, 1000);
+  }
